35d09fc9329c32063352e762c0d81b082108f059
kupns <kupns@mail.ru>
2021-03-27 22:13:12 +0300

    add const skip iterator

---
+++ lib/stego/skip.hpp
@@ -0,0 +1,43 @@
+
+#ifndef STEGO_SKIP_HPP
+#define STEGO_SKIP_HPP
+
+#include <iterator>
+#include <boost/iterator/iterator_facade.hpp>
+#include <boost/assert.hpp>
+
+template<typename _InputIterator,
+        typename __T = typename std::iterator_traits<_InputIterator>::value_type>
+struct ConstSkipIterator : public boost::iterator_facade<ConstSkipIterator<_InputIterator>, __T const, boost::forward_traversal_tag>
+{
+    using It = _InputIterator;
+
+    constexpr ConstSkipIterator(It i) : _i(i), _s(SIZE_MAX) { }
+
+    ConstSkipIterator(size_t s, It i) : _i(i), _s(s)
+    {
+        BOOST_ASSERT_MSG(_s, "Infinite cycle detected");
+    }
+
+    void increment()
+    {
+        std::advance(_i, _s);
+    }
+
+    __T const& dereference() const
+    {
+        return *_i;
+    }
+
+    bool equal(ConstSkipIterator o) const
+    {
+        if(std::distance(_i, o._i) < 0) return true;
+        return _i == o._i;
+    }
+
+private:
+    size_t _s;
+    It _i;
+};
+
+#endif //STEGO_SKIP_HPP
---
+++ tests/skip/tst_skiptestcase.cpp
@@ -0,0 +1,62 @@
+#include <QtTest>
+
+#include "stego/skip.hpp"
+
+class ConstSkipIteratorTestCase : public QObject
+{
+    Q_OBJECT
+
+public:
+    ConstSkipIteratorTestCase() = default;
+    ~ConstSkipIteratorTestCase() = default;
+
+private slots:
+    void initTestCase();
+    void initTestCase_data();
+
+    void init();
+
+    void skipTestCase_data();
+    void skipTestCase();
+
+    void cleanupTestCase();
+};
+
+void ConstSkipIteratorTestCase::initTestCase_data() { }
+
+void ConstSkipIteratorTestCase::initTestCase() { }
+
+void ConstSkipIteratorTestCase::init() { }
+
+void ConstSkipIteratorTestCase::skipTestCase_data() {
+    QTest::addColumn<int>("skip");
+    QTest::addColumn<int>("sum");
+    QTest::addColumn<QList<int>>("container");
+
+    auto l = QList<int>{1, 2, 3, 4, 5, 6};
+    QTest::newRow("Small list range skip 3") << 4 << 6 << l;
+    QTest::newRow("Small list range skip 2") << 2 << 9 << l;
+    QTest::newRow("Small list range skip 1") << 1 << 21 << l;
+}
+
+void ConstSkipIteratorTestCase::skipTestCase()
+{
+    QFETCH(QList<int>, container);
+    QFETCH(int, sum);
+    QFETCH(int, skip);
+
+    using T = QList<int>::ConstIterator;
+
+    auto actual = std::accumulate(
+            ConstSkipIterator(skip, container.cbegin()),
+            ConstSkipIterator(container.cend()), 0);
+    QCOMPARE(actual, sum);
+}
+
+void ConstSkipIteratorTestCase::cleanupTestCase()
+{
+}
+
+QTEST_APPLESS_MAIN(ConstSkipIteratorTestCase)
+
+#include "tst_skiptestcase.moc"
