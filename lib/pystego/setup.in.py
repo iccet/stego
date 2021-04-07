162d765b07aa82eb7838b9982ac06898dc2b4eaf
kupns <kupns@mail.ru>
2021-04-07 14:15:02 +0300

    add cmake python build

--- lib/pystego/setup.in.py
+++ lib/pystego/setup.in.py
@@ -3,17 +3,26 @@ from os import path
 from glob import glob
 
 
+def remove_namespace(f):
+    def wrapper(p: str):
+        return f(p.replace('::', ''))
+
+    return wrapper
+
+
+@remove_namespace
 def split(_path: str):
     return _path.split(';')
 
 
 source_files = glob(path.join(path.dirname(path.realpath(__file__)), "*.cpp"))
+libraries = split('${LINKED_LIBRARIES}')
 
 qt_include = map(split, ['${Qt5Core_INCLUDE_DIRS}', '${Qt5Gui_INCLUDE_DIRS}'])
 stg_extension = Extension('${PROJECT_NAME}',
                           include_dirs=['${CMAKE_SYSTEM_INCLUDE_PATH}', '${Stg_SOURCE_DIR}'] + sum(qt_include, []),
                           library_dirs=['${CMAKE_SYSTEM_LIBRARY_PATH}', '${Stg_BINARY_DIR}'],
-                          libraries=['Stg', 'Qt5Core', 'Qt5Gui'],
+                          libraries=libraries,
                           sources=source_files)
 
 if __name__ == '__main__':
