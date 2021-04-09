17c48fc95e17584e054f6c6838e1617891b4c95a
kupns <kupns@mail.ru>
2021-04-09 17:37:48 +0300

    add compiler flags

--- lib/pystego/setup.in.py
+++ lib/pystego/setup.in.py
@@ -1,5 +1,7 @@
 from typing import List
+
 from distutils.core import setup, Extension
+import sysconfig
 from os import path
 from glob import glob
 
@@ -18,12 +20,13 @@ def split(_path: str) -> List[str]:
 
 source_files = glob(path.join(path.dirname(path.realpath(__file__)), "*.cpp"))
 libraries = split('${LINKED_LIBRARIES}')
-
+extra_compile_args = ["-std=c++${CMAKE_CXX_STANDARD}"] + "${CMAKE_CXX_FLAGS}".strip().split(' ')
 qt_include = map(split, ['${Qt5Core_INCLUDE_DIRS}', '${Qt5Gui_INCLUDE_DIRS}'])
 stg_extension = Extension('${PROJECT_NAME}',
                           include_dirs=['${CMAKE_SYSTEM_INCLUDE_PATH}', '${Stg_SOURCE_DIR}'] + sum(qt_include, []),
                           library_dirs=['${CMAKE_SYSTEM_LIBRARY_PATH}', '${Stg_BINARY_DIR}'],
                           libraries=libraries,
+                          extra_compile_args=extra_compile_args + sysconfig.get_config_var('CFLAGS').split(),
                           sources=source_files)
 
 if __name__ == '__main__':
@@ -32,6 +35,14 @@ if __name__ == '__main__':
           description='',
           author='',
           author_email='',
+          platforms=['Mac OSX', 'POSIX', 'Windows'],
+          classifiers=[
+              'Intended Audience :: Developers',
+              'Operating System :: MacOS :: MacOS X',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Programming Language :: Python',
+          ],
           url='https://github.com/iccet/stego',
           long_description='''''',
           license='MIT',
